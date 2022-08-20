4ef1d97fd4933a44b3514741a1bea577a6fd6f60
Ismaelkane <105006162+Ismaelkane@users.noreply.github.com>
2022-08-20 08:18:35 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementsByClassName('menu')
-
-function toggleButton() {
-    navList.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click', toggleButton)
